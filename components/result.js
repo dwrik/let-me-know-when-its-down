574cbe475fe504f663e45b59dbf882303057a713
Wrik Das <wrikdas22@gmail.com>
2022-05-28 23:27:14 +0530

    implemented api for downtime check & component for check results

---
+++ components/result.js
@@ -0,0 +1,41 @@
+import { parse } from 'tldts'
+
+// helper
+function capitalize(string) {
+  return string[0].toUpperCase() + string.substring(1)
+}
+
+function Statistic({ property, value }) {
+  return (
+    <div className='d-flex justify-content-between'>
+      <b>{property}</b>
+      <span className=''>{value ?? 'N/A'}</span>
+    </div>
+  )
+}
+
+export default function Result({ result }) {
+  if (result === 'Checking') {
+    return <p className='lead mt-5'>{result}...</p>
+  }
+
+  const {status, statusText, host } = result
+  const name = capitalize(parse(host).domainWithoutSuffix)
+
+  return (
+    <div className='card mt-5 border-dark col-8 col-lg-6 col-xl-5'>
+      <div className='card-body'>
+        <h3 className='mb-3'>{name}</h3>
+        <Statistic property='Status code' value={status} />
+        <Statistic property='Status text' value={statusText} />
+        <Statistic property='Host name' value={host} />
+        <p className='mt-4 mb-0'>
+          {status >= 200 && status < 400
+            ? <b className='text-success'>&#9679; {name} is up and reachable by us.</b>
+            : <b className='text-danger'>&#9679; {name} is likely down at this time!</b>
+          }
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ pages/api/check.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export default async function handler(req, res) {
+  try {
+    const response = await axios.get(req.query.domain, { timeout: 8000 })
+    res.status(200).json({
+      status: response.status,
+      statusText: response.statusText,
+      host: response.request.host,
+    })
+  } catch ({ request, response }) {
+    const status = response ? response.status : null
+    const statusText = response ? response.statusText : null
+    const host = response ? response.config.url : request._currentUrl
+    res.status(200).json({ status, statusText, host })
+  }
+}
