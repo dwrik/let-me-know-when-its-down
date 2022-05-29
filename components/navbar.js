ad7f03c102784cae1b2c7c3b2977797786bb5e1f
Wrik Das <wrikdas22@gmail.com>
2022-05-29 19:21:01 +0530

    refactored styles

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-function NavItem({ href, text, pathname}) {
+function NavItem({ href, text, pathname, index, array }) {
   const isActive = pathname === href
   return (
-    <li className='me-4 pb-2'>
+    <li className={`${index !== array.length - 1 ? 'me-4' : null } pb-2`}>
       <Link href={href}>
         <a className={`${isActive ? 'text-dark' : 'text-black-50'} text-decoration-none`}>
           {text ? text : 'Home'}
@@ -20,11 +20,13 @@ export default function Navbar() {
 
   return (
     <nav className='d-flex justify-content-center justify-content-sm-end'>
-      <ul className='list-unstyled d-flex justify-content-between pt-4 pe-0'>
-        {navItems.map((item) => (
+      <ul className='list-unstyled d-flex m-4'>
+        {navItems.map((item, index, array) => (
           <NavItem
             key={item}
             text={item}
+            index={index}
+            array={array}
             href={'/' + item.toLowerCase().replace(' ', '')}
             pathname={router.pathname}
           />
