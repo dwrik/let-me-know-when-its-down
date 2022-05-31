952b9533b0de68eff1e02b6ac368312507d49e6e
Wrik Das <wrikdas22@gmail.com>
2022-05-31 13:09:55 +0530

    added website model & updated user model for many to many relations

--- models/website.js
+++ models/website.js
@@ -1,31 +1,33 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    maxlength: 60,
-  },
-  passwordHash: {
+const websiteSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true,
   },
-  websites: {
+  domain: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  users: {
     type: [{
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Website',
+      ref: 'User',
     }],
     default: [],
   }
 })
 
-userSchema.set('toJSON', {
+websiteSchema.set('toJSON', {
   transform: function (doc, retObj) {
     retObj.id = retObj._id
     delete retObj._id
     delete retObj.__v
-    delete retObj.passwordHash
   }
 })
 
-export default mongoose.models.User || mongoose.model('User', userSchema)
+export default mongoose.models.Website || mongoose.model('Website', websiteSchema)
