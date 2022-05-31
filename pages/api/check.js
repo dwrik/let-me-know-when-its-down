4b0f0b376546c8257d562f59d9ec1fe8f4c2b7bd
Wrik Das <wrikdas22@gmail.com>
2022-05-31 20:19:28 +0530

    refactored url checking to separate lib

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,17 +1,6 @@
-import axios from 'axios'
+import urlCheck from '../../lib/urlCheck'
 
 export default async function handler(req, res) {
-  try {
-    const response = await axios.get(req.body.url, { timeout: 10000 })
-    res.status(200).json({
-      status: response.status,
-      statusText: response.statusText,
-      host: response.request.host,
-    })
-  } catch ({ request, response }) {
-    const status = response ? response.status : null
-    const statusText = response ? response.statusText : null
-    const host = response ? response.config.url : request._currentUrl
-    res.status(200).json({ status, statusText, host })
-  }
+  const result = await urlCheck(req.body.url)
+  res.status(200).json(result)
 }
