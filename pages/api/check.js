53ce811f29f005b46fae1e3fd177afe02bed186b
Wrik Das <wrikdas22@gmail.com>
2022-05-31 13:03:10 +0530

    removed console.log statements

--- pages/api/check.js
+++ pages/api/check.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get(req.body.domain, { timeout: 10000 })
+    const response = await axios.get(req.body.url, { timeout: 10000 })
     res.status(200).json({
       status: response.status,
       statusText: response.statusText,
@@ -12,7 +12,6 @@ export default async function handler(req, res) {
     const status = response ? response.status : null
     const statusText = response ? response.statusText : null
     const host = response ? response.config.url : request._currentUrl
-    console.log(status, statusText, host)
     res.status(200).json({ status, statusText, host })
   }
 }
